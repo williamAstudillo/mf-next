dddc39504f3dd9f76a6aa549483db3aaa5b74aae
William Astudillo <william.astudillo@pragma.com.co>
2024-12-13 16:05:53 -0500

    feat : add mf home

---
+++ home/src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        soy el microfront de home
+        <a href="/mf1">go to MF1</a>
+      </main>
+    </div>
+  );
+}
